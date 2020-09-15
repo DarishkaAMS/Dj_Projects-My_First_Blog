6c9b45683a441246437806ec2008cb0f1c08b4d8
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-09-15 07:20:20 +0300

    Add Blog basics

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on') #we have arranged all the posts by their creation date. + he ( – ) sign before the created_on signifies the latest post would be at the top and so on.
+    template_name = 'index.html' 
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
